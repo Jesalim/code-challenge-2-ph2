d0a0c7ca3d9573104541a059267c48293f6ad9a6
Jesalim <jessyculture@gmail.com>
2023-01-23 10:36:59 +0300

    done

---
+++ src/components/App/App.js
@@ -0,0 +1,18 @@
+import "./App.css";
+import NavBar from "../Navbar";
+import DataFetching from "../Datafetct";
+import BotCollection from "../BotCollection";
+import BotArmy from "../YourBotArmy";
+function App() {
+  return (
+    <div>
+      <NavBar />
+      <BotArmy/>
+      <DataFetching/>
+      
+      <BotCollection/>
+    </div>
+  );
+}
+
+export default App;
